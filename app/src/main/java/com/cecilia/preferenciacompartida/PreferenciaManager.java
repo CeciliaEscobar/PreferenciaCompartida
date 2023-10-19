612ad85902bde8784bc9b8b66dd10e7c490ec028
CeciliaEscobar <escobarceciramoa@gmail.com>
2023-10-18 22:34:09 -0300

    Implementacion Finalizada

---
+++ app/src/main/java/com/cecilia/preferenciacompartida/PreferenciaManager.java
@@ -0,0 +1,36 @@
+package com.cecilia.preferenciacompartida;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class PreferenciaManager {
+    Context context;
+
+    //constructor de la clase que recibe context de otra activity
+    PreferenciaManager (Context contextRecibido){
+        this.context = contextRecibido;
+    }
+
+    public void salvarLoginDetalles(String email, String password){
+
+        SharedPreferences sharedPreferences = context.getSharedPreferences("LoginDatos", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("Email", email);
+        editor.putString("Password", password);
+        editor.commit();
+    }
+
+    public String getEmail(){
+        SharedPreferences sharedPreferences = context.getSharedPreferences("LoginDatos", Context.MODE_PRIVATE);
+        return sharedPreferences.getString("Email", "");
+    }
+
+    public boolean isUsuarioLogedOut(){
+        SharedPreferences sharedPreferences = context.getSharedPreferences("LoginDatos", Context.MODE_PRIVATE);
+        boolean isEmailVacio = sharedPreferences.getString("Email", "").isEmpty();
+        boolean isPasswordVacio = sharedPreferences.getString("Password", "").isEmpty();
+
+        return isEmailVacio || isPasswordVacio;
+    }
+
+}
